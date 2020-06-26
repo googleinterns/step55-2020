8626816274fdbd2695e8ddcaf78835e13825e60d
saramissak <sarammissak@gmail.com>
2020-06-26 15:41:21 +0000

    Allows the user to switch between tabs

--- src/main/webapp/js/createGame.js
+++ src/main/webapp/js/createGame.js
@@ -1,30 +1,36 @@
 function addNewStage() {
-    const activeStages = document.getElementsByClassName('activeStage');
-    activeStages[0].classList.remove('activeStage');
-    const stagesUl = document.getElementById('stages');
-    const numStages = stagesUl.getElementsByTagName('input').length;
+    const activeStages = document.getElementsByClassName('activeStage')[0];
+    activeStages.classList.remove('activeStage');
+    const divToHide = activeStages.classList[0];
+    document.getElementById(divToHide + 'Hints').classList.add('hidden');
+    const stagesList = document.getElementById('stages');
+    const numStages = stagesList.getElementsByTagName('input').length;
 
     const newStage = document.createElement('input');
     newStage.value = 'Stage ' + (numStages + 1);
     newStage.type= 'button';
     newStage.className = 'stage' + (numStages + 1) + ' activeStage';
 
+    newStage.addEventListener("click", function(){
+        setActive('stage' + (numStages + 1));
+    });
+
     const a = document.createElement('a');
     a.href = '#stage' + (numStages + 1);
 
     a.appendChild(newStage);
-    stagesUl.appendChild(a);
+    stagesList.appendChild(a);
 
     const newStageHints = document.createElement('div');
     newStageHints.id = newStage.classList[0] + 'Hints';
     newStageHints.innerHTML = "<input type='text' placeholder='Starter (spawn location and first hint)' id='starter-hint' required>" +
-                                "<input type='text' placeholder='Hint 1' id='hint1'>"
+                                "<input type='text' placeholder='Hint 1' id='hint1'>";
 
     document.getElementById('hints').appendChild(newStageHints);
 }
 
 function addNewHint() {
-    const activeStageNum = (document.getElementsByClassName('activeStage')[0].classList)[0] + 'Hints';
+    const activeStageNum = (document.getElementsByClassName('activeStage')[0]).classList[0] + 'Hints';
     const activeHints = document.getElementById(activeStageNum);
     const numHints = activeHints.getElementsByTagName('input').length;
 
@@ -35,8 +41,14 @@ function addNewHint() {
     activeHints.appendChild(newHint);
 }
 
-function setActive() {
-    const activeStages = document.getElementsByClassName('activeStage');
-    activeStages[0].classList.remove('activeStage');
-
+function setActive(toSetActive) {
+    const activeStages = document.getElementsByClassName('activeStage')[0];
+    if (activeStages.classList[0] == toSetActive) return;
+    activeStages.classList.remove('activeStage');
+    const divToHide = activeStages.classList[0];
+    document.getElementById(divToHide + 'Hints').classList.add('hidden');
+    
+    const newActiveStage = document.getElementsByClassName(toSetActive)[0];
+    newActiveStage.classList.add('activeStage');
+    document.getElementById(toSetActive + 'Hints').classList.remove('hidden');
 }
--- target/portfolio-1/js/createGame.js
+++ target/portfolio-1/js/createGame.js
@@ -1,47 +1,55 @@
 function addNewStage() {
-    const activeStages = document.getElementsByClassName('activeStage');
-    activeStages[0].classList.remove('activeStage');
-    const stagesUl = document.getElementById('stages');
-    const numStages = stagesUl.getElementsByTagName('input').length;
+    const activeStages = document.getElementsByClassName('activeStage')[0];
+    activeStages.classList.remove('activeStage');
+    const divToHide = activeStages.classList[0];
+    document.getElementById(divToHide + 'Hints').classList.add('hidden');
+    const stagesList = document.getElementById('stages');
+    const numStages = stagesList.getElementsByTagName('input').length;
 
     const newStage = document.createElement('input');
     newStage.value = 'Stage ' + (numStages + 1);
     newStage.type= 'button';
     newStage.className = 'stage' + (numStages + 1) + ' activeStage';
 
+    newStage.addEventListener("click", function(){
+        setActive('stage' + (numStages + 1));
+    });
+
     const a = document.createElement('a');
     a.href = '#stage' + (numStages + 1);
 
     a.appendChild(newStage);
-    stagesUl.appendChild(a);
+    stagesList.appendChild(a);
 
     const newStageHints = document.createElement('div');
     newStageHints.id = newStage.classList[0] + 'Hints';
     newStageHints.innerHTML = "<input type='text' placeholder='Starter (spawn location and first hint)' id='starter-hint' required>" +
-                                "<input type='text' placeholder='Hint 1' id='hint1'>"
+                                "<input type='text' placeholder='Hint 1' id='hint1'>";
 
     document.getElementById('hints').appendChild(newStageHints);
 }
 
 function addNewHint() {
-    const activeStageNum = (document.getElementsByClassName('activeStage')[0].classList)[0] + 'Hints';
-    console.log(activeStageNum)
+    const activeStageNum = (document.getElementsByClassName('activeStage')[0]).classList[0] + 'Hints';
     const activeHints = document.getElementById(activeStageNum);
-    console.log(activeHints)
     const numHints = activeHints.getElementsByTagName('input').length;
-    console.log(numHints)
 
     const newHint = document.createElement('input');
     newHint.id = 'hint' + (numHints);
     newHint.placeholder = 'Hint ' + (numHints);
     newHint.type= 'text';
     activeHints.appendChild(newHint);
-    console.log(newHint)
-    console.log(activeHints)
 }
 
-function setActive() {
-    const activeStages = document.getElementsByClassName('activeStage');
-    activeStages[0].classList.remove('activeStage');
-
+function setActive(toSetActive) {
+    const activeStages = document.getElementsByClassName('activeStage')[0];
+    if (activeStages.classList[0] == toSetActive) return;
+    activeStages.classList.remove('activeStage');
+    const divToHide = activeStages.classList[0];
+    document.getElementById(divToHide + 'Hints').classList.add('hidden');
+    
+    const newActiveStage = document.getElementsByClassName(toSetActive)[0];
+    newActiveStage.classList.add('activeStage');
+    document.getElementById(toSetActive + 'Hints').classList.remove('hidden');
+    console.log(document.getElementsByClassName('hidden'))
 }
