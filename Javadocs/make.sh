a0a1de0f67f1a3b5e8cf7797985029525e9ed467
Lawrence <ldorianchen@gmail.com>
2020-06-26 16:47:54 +0000

    Remove javadocs directory from repository

--- Javadocs/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -r ~/step55-2020/src/main/java/com/google/sps/data/. javafiles
-cp -r ~/step55-2020/src/main/java/com/google/sps/servlets/. javafiles
-
-javadoc -sourcepath ./javafiles -d ./output -subpackages .
