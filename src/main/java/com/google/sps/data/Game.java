677f42adc42f7177e8826caa63bf7ff07979fbc9
Lawrence <ldorianchen@gmail.com>
2020-06-26 15:30:18 +0000

    Add Javadoc documentation to all methods and classes

--- src/main/java/com/google/sps/data/Game.java
+++ src/main/java/com/google/sps/data/Game.java
@@ -16,7 +16,9 @@ package com.google.sps.data;
 
 import java.util.ArrayList;
 
-// Represents the data of a single Game
+/**
+* Represents the static data of a single game.
+*/
 public class Game {
     private final String gameID;
     private final String gameName;
@@ -30,6 +32,9 @@ public class Game {
     private final int numDifficultyVotes;
     private final int totalDifficulty;
 
+    /**
+    * Builder class used to create Game objects.
+    */
     public static class Builder {
         // Required parameters
         private final String gameID;
@@ -46,61 +51,121 @@ public class Game {
         private int numDifficultyVotes = -1;
         private int totalDifficulty = -1;
 
+        /**
+        * Constructor that sets the game ID and the game name.
+        * @param gameID the unique code used to identify this specific game.
+        * @param gameName a string representing the game's name.
+        */
         public Builder(String gameID, String gameName) {
             this.gameID = gameID;
             this.gameName = gameName;
         }
 
+        /**
+        * Sets the game's description, which is displayed on the game's page.
+        * @param val a String representing the new description.
+        * @return a Builder object with the modified description.
+        */
         public Builder setGameDescription(String val) {
             this.gameDescription = val;
             return this;
         }
         
+        /**
+        * Sets the number of stages in the game.
+        * @param val an int representing the number of stages.
+        * @return a Builder object with the modified number of stages.
+        */
         public Builder setNumStages(int val) {
             this.numStages = val;
             return this;
         }
 
+        /**
+        * Specifies which stages are in the game.
+        * @param val an ArrayList of String representing the list of stages in the game.
+        *            Each stage is represented by its ID.
+        * @return a Builder object with the modified list of stages.
+        */
         public Builder setStages(ArrayList<String> val) {
             this.stages = val;
             return this;
         }
 
+        /**
+        * Sets the number of times the game has been played.
+        * @param val an int representing the number of times the game has been played.
+        * @return a Builder object with the modified number of times played.
+        */
         public Builder setNumTimesPlayed(int val) {
             this.numTimesPlayed = val;
             return this;
         }
 
+        /**
+        * Sets the number of times the game has been played to completion.
+        * @param val an int representing the number of times this game has been finished.
+        * @return a Builder object with the modified number of times finished.
+        */
         public Builder setNumTimesFinished(int val) {
             this.numTimesFinished = val;
             return this;
         }
 
+        /**
+        * Sets the number of star votes this game has received.
+        * @param val an int representing the number of times this game has been voted on.
+        * @return a Builder object with the modified number of star votes.
+        */
         public Builder setNumStarVotes(int val) {
             this.numStarVotes = val;
             return this;
         }
 
+        /**
+        * Sets the total number of stars this game has received.
+        * @param val an int representing the total number of stars this game has received.
+        * @return a Builder object with the modified total number of stars.
+        */
         public Builder setTotalStars(int val) {
             this.totalStars = val;
             return this;
         }
 
+        /**
+        * Sets the number of difficulty votes this game has received.
+        * @param val an int representing the number of times this game has received a difficulty vote.
+        * @return a Builder object with the modified number of difficulty votes.
+        */
         public Builder setNumDifficultyVotes(int val) {
             this.numDifficultyVotes = val;
             return this;
         }
 
+        /**
+        * Sets the sum of all difficulty votes received by this game (1 = Easy, 2 = Medium, 3 = Hard).
+        * @param val an int representing the total sum of difficulty votes.
+        * @return a Builder object with the modified total difficulty.
+        */
         public Builder setTotalDifficulty(int val) {
             this.totalDifficulty = val;
             return this;
         }
 
+        /**
+        * Gets the Game object created by this Builder.
+        * @return a Game object with the properties specified in the Builder.
+        */
         public Game build() {
             return new Game(this);
         }
     }
     
+    /**
+    * Constructor that converts a Builder into a Game.
+    * @param builder the Builder object to be converted.
+    * @see Builder#build()
+    */
     private Game(Builder builder) {
         this.gameID = builder.gameID;
         this.gameName = builder.gameName;
--- src/main/java/com/google/sps/servlets/CreateGameServlet.java
+++ src/main/java/com/google/sps/servlets/CreateGameServlet.java
@@ -33,7 +33,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Takes the created game information and puts the new game in datastore */
+/**
+* Servlet that processes a game once it has been created.
+*/
 @WebServlet("/create-game-data")
 public class CreateGameServlet extends HttpServlet {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
@@ -43,35 +45,86 @@ public class CreateGameServlet extends HttpServlet {
         response.sendRedirect("/index.html");
     }
 
+    /**
+    * Retrieves the game's name.
+    * @param request the HttpServletRequest of the doPost.
+    * @return a String representing the name/title of the game.
+    */
     private String getName(HttpServletRequest request) {
         return request.getParameter("gameName");
     }
 
+    /**
+    * Retrieves the game's description.
+    * @param request the HttpServletRequest of the doPost.
+    * @return a String representing the description of the game.
+    */
     private String getDescription(HttpServletRequest request) {
         return request.getParameter("gameDescription");
     }
 
     // Below: TODO once game creation form is done
+    /**
+    * Retrieves the number of stages in the game.
+    * @param request the HttpServletRequest of the doPost.
+    * @return an int representing the number of stages.
+    */
     private int getNumStages(HttpServletRequest request) {
         return -1;
     }
 
+    /**
+    * Retrieves the starting location of each stage.
+    * @param request the HttpServletRequest of the doPost.
+    * @return an ArrayList, where the ith element is a Coordinates representing
+    *         the starting location of the ith stage.
+    * @see com.google.sps.data.Coordinates
+    */
 	private ArrayList<Coordinates> getStageSpawnLocations(HttpServletRequest request) {
         return null;
     }
 
+    /**
+    * Retrieves the starting hint for each stage.
+    * @param request the HttpServletRequest of the doPost.
+    * @return an ArrayList, where the ith element is a String representing
+    *         the starting hint for the ith stage.
+    */
     private ArrayList<String> getStageStarterHints(HttpServletRequest request) {
         return null;
     }
 
+    /**
+    * Retrieves the number of hints (excluding the starter hint) in each stage.
+    * @param request the HttpServletRequest of the doPost.
+    * @return an ArrayList, where the ith element is an int representing
+    *         the number of hints in the ith stage.
+    */
     private ArrayList<Integer> getNumHints(HttpServletRequest request) {
         return null;
     }
 
+    /**
+    * Retrieves the coordinates of each hint in each stage.
+    * @param request the HttpServletRequest of the doPost.
+    * @return an ArrayList, where the ith element is a list of
+    *         Coordinates of the hints on the ith stage. The jth
+    *         element of this list represents the Coordinates of
+    *         the jth hint of the ith stage.
+    * @see com.google.sps.data.Coordinates
+    */
     private ArrayList<ArrayList<Coordinates>> getHintCoordinates(HttpServletRequest request) {
         return null;
     }
 
+    /**
+    * Retrieves the text of each hint in each stage.
+    * @param request the HttpServletRequest of the doPost.
+    * @return an ArrayList, where the ith element is a list of
+    *         Strings of the hints on the ith stage. The jth
+    *         element of this list represents the text of
+    *         the jth hint of the ith stage.
+    */
     private ArrayList<ArrayList<String>> getHintText(HttpServletRequest request) {
         return null;
     }
