ff8a79f1b940d787b3170ba9551aab0ecc09d8e9
Lawrence <ldorianchen@gmail.com>
2020-07-08 21:05:58 +0000

    Add servlet for updating the progress of a user within a game

--- src/main/java/com/google/sps/servlets/UpdateSinglePlayerProgressServlet.java
+++ src/main/java/com/google/sps/servlets/UpdateSinglePlayerProgressServlet.java
@@ -36,25 +36,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
-* Servlet that manages retrieving/creating/resetting the progress of a user in game.
+* Servlet that updates the current progress of a user in a game.
 */
-@WebServlet("/load-singleplayerprogress-data")
-public class LoadSinglePlayerProgressServlet extends HttpServlet {
+@WebServlet("/update-singleplayerprogress-data")
+public class UpdateSinglePlayerProgressServlet extends HttpServlet {
     UserService userService = UserServiceFactory.getUserService();
     DatastoreManager datastoreManager = new DatastoreManager();
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        SinglePlayerProgress res = null;
-        if(userService.isUserLoggedIn()) {
-            String userID = userService.getCurrentUser().getUserId();
-            String gameID = request.getParameter("gameID");
-            res = datastoreManager.retrieveSinglePlayerProgress(userID, gameID);
-        }
-        
-        String json = new Gson().toJson(res);
-        response.getWriter().println(json);
-    }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -63,12 +50,42 @@ public class LoadSinglePlayerProgressServlet extends HttpServlet {
         }
         String userID = userService.getCurrentUser().getUserId();
         String gameID = request.getParameter("gameID");
+
         SinglePlayerProgress.Builder progressBuilder = new SinglePlayerProgress.Builder(userID, gameID);
-        Game game = datastoreManager.retrieveGame(gameID);
-        Stage firstStage = datastoreManager.retrieveStage(game.getStages().get(0));
-        progressBuilder.setLocation(firstStage.getStartingLocation());
-        progressBuilder.setHintsFound(new ArrayList<>());
-        progressBuilder.setStageID(firstStage.getStageID());
+        SinglePlayerProgress oldProgress = datastoreManager.retrieveSinglePlayerProgress(userID, gameID);
+        progressBuilder.setLocation(oldProgress.getLocation());
+        progressBuilder.setHintsFound(oldProgress.getHintsFound());
+        progressBuilder.setStageID(oldProgress.getStageID());
+
+        int type = Integer.parseInt(request.getParameter("type"));
+        if(type == 0) { // the user's location has changed
+            double latitude = Double.parseDouble(request.getParameter("latitude"));
+            double longitude = Double.parseDouble(request.getParameter("longitude"));
+            progressBuilder.setLocation(new Coordinates(latitude, longitude));
+        } else if(type == 1) { // the user has found a hint
+            String hintID = request.getParameter("hintID");
+            ArrayList<String> oldHints = oldProgress.getHintsFound();
+            oldHints.add(hintID);
+            progressBuilder.setHintsFound(oldHints);
+        } else { // the user has completed a stage
+            Game game = datastoreManager.retrieveGame(gameID);
+            
+            String currentStage = oldProgress.getStageID();
+            ArrayList<String> stages = game.getStages();
+            // get index of current stage
+            for(int i = 0; i < stages.size(); i++) {
+                if(stages.get(i).equals(currentStage)) {
+                    // set the new stage id to be the next index
+                    if(i == stages.size()-1) {
+                        // already at last stage
+                        progressBuilder.setStageID("N/A");
+                    } else {
+                        progressBuilder.setStageID(stages.get(i+1));
+                    }
+                    break;
+                }
+            }
+        }
         datastoreManager.createOrReplaceSinglePlayerProgress(progressBuilder.build());
     }
 }
