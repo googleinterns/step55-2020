2be615e0d7ee2cbf5b1c386a83d20f45b502baa5
saramissak <sarammissak@gmail.com>
2020-07-09 16:58:42 +0000

    remove system.out.println

--- src/main/java/com/google/sps/data/SinglePlayerProgress.java
+++ src/main/java/com/google/sps/data/SinglePlayerProgress.java
@@ -57,7 +57,6 @@ public class SinglePlayerProgress {
         */
         public Builder setLocation(Coordinates location) {
             this.location = location;
-            // System.out.println("builder setLocatio" + location);
             return this;
         }
 
@@ -99,7 +98,6 @@ public class SinglePlayerProgress {
         this.userID = builder.userID;
         this.gameID = builder.gameID;
         this.location = builder.location;
-        // System.out.println("builder setLocation" + this.location);
         this.hintsFound = builder.hintsFound;
         this.stageID = builder.stageID;
     }
