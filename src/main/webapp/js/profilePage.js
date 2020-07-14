9649caf0586dfd7678d72689a795ce1eedff5fb5
Lawrence <ldorianchen@gmail.com>
2020-07-14 22:19:26 +0000

    Make changes suggested by Sara

--- src/main/webapp/js/profilePage.js
+++ src/main/webapp/js/profilePage.js
@@ -41,25 +41,33 @@ async function isTaken(desiredUsername) {
   return result;
 }
 
+/**
+* Runs a series of checks (length, characters used, availability) to see if this username is valid.
+* @param {string} desiredUsername the username to be checked.
+* @return {string} 'Available' if this username is good; otherwise a string describing the issue.
+*/
 async function getAvailabilityText(desiredUsername) {
   if(desiredUsername.length == 0) {
-    return 'Username must be at least 1 character'
+    return 'Username must be at least 1 character';
   }
   if(desiredUsername.length > 20) {
-    return 'Username cannot be more than 20 characters'
+    return 'Username cannot be more than 20 characters';
   }
   let pattern = new RegExp(/^[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz._0123456789]*$/);
   if(!pattern.test(desiredUsername)) {
     return 'Only letters, digits, underscores, and periods are allowed';
   }
   if(await isTaken(desiredUsername)) {
-    return 'Username is already taken'
+    return 'Username is already taken';
   }
   return 'Available';
 }
 
+/**
+* Displays the result of getAvailabilityText in the div element under the username input box.
+*/
 async function displayAvailability() {
-  desiredUsername = document.getElementById('userName').value;
+  let desiredUsername = document.getElementById('userName').value;
   let availabilityBox = document.getElementById('username-availability-message');
   availabilityBox.innerText = await getAvailabilityText(desiredUsername);
   if(availabilityBox.innerText == 'Available') {
@@ -70,14 +78,14 @@ async function displayAvailability() {
 }
 
 async function submitUsername() {
-  desiredUsername = document.getElementById('userName').value;
+  let desiredUsername = document.getElementById('userName').value;
   let availabilityBox = document.getElementById('username-availability-message');
   await displayAvailability();
   if(availabilityBox.innerText == 'Available') {
     const params = new URLSearchParams();
     params.append('userName', desiredUsername);
     await fetch('/create-username-data', {method: 'post', body: params});
-    window.location = '/profilePage.html'
+    window.location = '/profilePage.html';
   } else {
     alert(availabilityBox.innerText);
   }
